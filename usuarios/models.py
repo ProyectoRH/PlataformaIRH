5fb28651c506cb1b9f4c2dd924ccf4347ac06d65
Jorge Vergara <javebratt@gmail.com>
2015-01-28 16:23:01 -0500

    Agregando campos adicionales al modulo usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -3,6 +3,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Pais(models.Model):
+	pais = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.pais
+
+
+class Ciudad(models.Model):
+	ciudad = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.ciudad
+
+
 class Institucion(models.Model):
 	institucion = models.CharField(max_length=140)
 	nombre_nucleo = models.CharField(max_length=255)
@@ -29,6 +43,11 @@ class UserProfile(models.Model):
 	usuario = models.OneToOneField(User)
 	institucion = models.ForeignKey(Institucion, null=True, blank=True)
 	tipo_usuario = models.ForeignKey(TipoUsuario, null=True, blank=True)
+	funcion = models.CharField(max_length=200, blank=True, null=True)
+	pais = models.ForeignKey(Pais)
+	ciudad = models.ForeignKey(Ciudad)
+	
+
 
 	def __unicode__(self):
 		return "%s %s - %s" % (self.usuario.first_name, self.usuario.last_name, self.usuario.username)
