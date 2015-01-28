743bc43c134bd6f09b7a4e1cf590c1b34349e107
Jorge Vergara <javebratt@gmail.com>
2015-01-28 16:46:50 -0500

    Integración de inlines

--- localizacion/admin.py
+++ localizacion/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from leaflet.admin import LeafletGeoAdmin
+from .models import Localizacion
+
+from documentos_digitales.models import DocumentoDigital
+from documentos_shape.models import DocumentoShape
+
+
+
+
+class DocumentoDigitalInline(admin.TabularInline):
+	model = DocumentoDigital
+	extra = 3
+
+class DocumentoShapeInline(admin.TabularInline):
+	model = DocumentoShape
+	extra = 3
+
+class LocalizacionAdmin(admin.ModelAdmin):
+	inlines = [DocumentoDigitalInline, DocumentoShapeInline]
+
+
+
+
+admin.site.register(Localizacion, LeafletGeoAdmin, LocalizacionAdmin)
--- localizacion/models.py
+++ localizacion/models.py
@@ -1,15 +1,20 @@
+# -*- encoding: utf-8 -*-
 from django.db import models
 
 from areatematicas.models import Areatematica
 from representacion.models import Representacion
 from zona.models import Zona, SubZona
 
+from djgeojson.fields import GeometryField
+
 
 class Localizacion(models.Model):
+	titulo = models.CharField(max_length=200)
 	area_tematica = models.ForeignKey(Areatematica)
 	representacion = models.ForeignKey(Representacion)
 	zona = models.ManyToManyField(Zona)
 	sub_zona = models.ManyToManyField(SubZona)
+	geom = GeometryField(verbose_name='Especifique ubicación')
 
 
 	def __unicode__(self):
