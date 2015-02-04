d1881a20ce3ea1e0fe32132f608ab700fc5cf438
reynaldo <reynaldovilla@gmail.com>
2015-02-04 10:58:18 -0500

    Modificacion de instalador de plataforma

--- install.sh
+++ install.sh
@@ -23,13 +23,17 @@ echo "Instalando librerias y adaptadores para Python y Postgres..."
 sudo apt-get install libpq-dev python-dev python-psycopg2
 echo "*********************************************************************"
 pip install -r requirements.txt
-read -r "El nombre de la base de datos: " dbname
+read -p "El nombre de la base de datos: " dbname
+export dbname=$dbname
 sudo echo "export dbname=$dbname">> /etc/profile
-read -r "El nombre del usuario postgres:" usname
+read -p "El nombre del usuario postgres: " usname
+export usname=$usname
 sudo echo "export usname=$usname">> /etc/profile
-read -r "La contraseña del usuario postgres:" uscontra
+read -p "La contraseña del usuario postgres: " uscontra
+export uscontra=$uscontra
 sudo echo "export uscontra=$uscontra">> /etc/profile
-read -r "La ip o nombre del servidor postgres:" ipdir
+read -p "La ip o nombre del servidor postgres: " ipdir
+export ipdir=$ipdir
 sudo echo "export ipdir=$ipdir">> /etc/profile
 
 echo "OK la instalcion ha terminado...."
