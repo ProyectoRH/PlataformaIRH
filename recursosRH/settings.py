66a14139d6d2706b197f0f0353e930c88c7c3cf1
Sergio Obredor <slobredor@gmail.com>
2015-02-10 15:08:47 -0500

    Rempazar texto plano por variables de ambiente en la base de datos

--- recursosRH/settings.py
+++ recursosRH/settings.py
@@ -96,12 +96,12 @@ TEMPLATE_CONTEXT_PROCESSORS = TCP+(
 DATABASES = {
         'default': {
             'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'recursosdb',                      # Or path to database file if using sqlite3.
+            'NAME': DBNAME,                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'recursosrh',
-            'PASSWORD': 'nmveaviieotf',
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
+            'USER': USNAME,
+            'PASSWORD': USCONTRA,
+            'HOST': IPDIR,                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '5432',                               # Set to empty string for default.
         }
     }
 
