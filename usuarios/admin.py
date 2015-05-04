b41a1d49eddc2f4f6060a65d8c835c004a19d0ec
Jorge Vergara <javebratt@gmail.com>
2015-05-04 00:03:09 -0500

    OK

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
 
-from .models import UserProfile, TipoUsuario
+from .models import UserProfile, Institucion, TipoUsuario
 
 
 class ProfileAdmin(admin.ModelAdmin):
 	fields = ('usuario', 'institucion', )
 
 
+admin.site.register(Institucion)
 admin.site.register(UserProfile, ProfileAdmin)
