8aba4dfa80ea9e0c638a75802689dcf060a25d12
Jorge Vergara <javebratt@gmail.com>
2015-05-04 11:07:10 -0500

    Modelos de apps de insituciones y nucleos

--- institucion/models.py
+++ institucion/models.py
@@ -5,9 +5,6 @@ from django.contrib.auth.models import User
 
 class Institucion(models.Model):
 	institucion = models.CharField(max_length=140)
-	imagen_banner = models.ImageField(upload_to = "static/uploads_nucleos/banner_images", null = True)
-	descripcion_nucleo = models.TextField()
-	logo_institucion = models.ImageField(upload_to = "static/logo_instituciones", null = True)
 
 	def __unicode__(self):
 		return self.institucion
--- nucleo/models.py
+++ nucleo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+from institucion.models import Institucion
+
+
+
+class Nucleo(models.Model):
+	institucion = models.ForeignKey(Institucion)
+	imagen_banner = models.ImageField(upload_to = "static/uploads_nucleos/banners_nucleos", null = True)
+	nucleo = models.CharField(max_length=200)
+	descripcion = models.TextField()
+	imagen_logo = models.ImageField(upload_to = "static/uploads_nucleos/logos_nucleos", null = True)
+
+	def __unicode__(self):
+		return self.institucion
