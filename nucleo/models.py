0906c8c934c5e8679c1427f2523cf2b70825b954
Jorge Vergara <javebratt@gmail.com>
2015-05-04 11:18:39 -0500

    Migraciones

--- nucleo/models.py
+++ nucleo/models.py
@@ -8,10 +8,10 @@ from institucion.models import Institucion
 
 class Nucleo(models.Model):
 	institucion = models.ForeignKey(Institucion)
-	imagen_banner = models.ImageField(upload_to = "static/uploads_nucleos/banners_nucleos", null = True)
 	nucleo = models.CharField(max_length=200)
+	imagen_banner = models.ImageField(upload_to = "static/uploads_nucleos/banners_nucleos", null = True)
 	descripcion = models.TextField()
 	imagen_logo = models.ImageField(upload_to = "static/uploads_nucleos/logos_nucleos", null = True)
 
 	def __unicode__(self):
-		return self.institucion
+		return self.nucleo
--- recursosRH/settings.py
+++ recursosRH/settings.py
@@ -71,7 +71,11 @@ INSTALLED_APPS = (
     'EventosOndas',
     'VideosOndas',
     'ImagenesOndas',
+    'institucion',
+    'nucleo',
 )
+
+AUTH_USER_MODEL = 'usuarios.Usuario'
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -106,7 +110,7 @@ DATABASES = {
             'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'recursosdb',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'recursosuser',
+            'USER': 'recursosrh',
             'PASSWORD': 'recursosrrhh',
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                               # Set to empty string for default.
