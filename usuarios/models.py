bd55b06dac3588d3d8fc6ee5eaac139b16d5f08c
Jorge Vergara <javebratt@gmail.com>
2015-05-04 12:05:46 -0500

    Eliminando nucleo de usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -10,7 +10,6 @@ from nucleo.models import Nucleo
 class Usuario(User):
     """User with app settings."""
     institucion = models.ForeignKey(Institucion)
-    nucleo = models.ForeignKey(Nucleo)
 
     # Use UserManager to get the create_user method, etc.
     objects = UserManager()
