45d75311f847d45650a4a10adec8c35ade6eea98
Jorge Vergara <javebratt@gmail.com>
2015-05-12 14:38:24 -0500

    Cambiando el backend para registrar por usuario y no por mail

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,49 +1,7 @@
-#-*- encoding: utf-8 -*-
-from django.db import models
-from django.conf import settings
-
-from django.contrib.auth.models import AbstractUser, UserManager
-
+from django.contrib.auth.models import AbstractUser
 from institucion.models import Institucion
-from nucleo.models import Nucleo
-from django.contrib.auth import models as auth_models
-
-
-class MyUserManager(UserManager):
-    def create_user(self, email, date_of_birth, password=None):
-        """
-        Creates and saves a User with the given email, date of
-        birth and password.
-        """
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email),
-            date_of_birth=date_of_birth,
-        )
-
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, date_of_birth, password):
-        """
-        Creates and saves a superuser with the given email, date of
-        birth and password.
-        """
-        user = self.create_user(email,
-            password=password,
-            date_of_birth=date_of_birth
-        )
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
 
 
 class Usuario(AbstractUser):
     """User with app settings."""
     institucion = models.ForeignKey(Institucion, null=True)
-
-    # Use UserManager to get the create_user method, etc.
-    objects = MyUserManager()
