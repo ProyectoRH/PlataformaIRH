be7b97fadf9f111a1c4ec96b28d82c2cff617025
Mario Bonfante Aldana <maxboal@gmail.com>
2015-07-14 12:47:28 -0500

    Variables de Entorno de vuelta a la normalidad.

--- recursosRH/settings.py
+++ recursosRH/settings.py
@@ -30,10 +30,10 @@ ALLOWED_HOSTS = []
 
 # OAUTH2_PROVIDER_APPLICATION_MODEL = 'oauth2_provider.Application'
 
-# DBNAME= os.environ.get('dbname')
-# USNAME= os.environ.get('usname')
-# USCONTRA= os.environ.get('uscontra')
-# IPDIR= os.environ.get('ipdir')
+DBNAME= os.environ.get('dbname')
+USNAME= os.environ.get('usname')
+USCONTRA= os.environ.get('uscontra')
+IPDIR= os.environ.get('ipdir')
 # # Application definition
 
 INSTALLED_APPS = (
@@ -108,11 +108,11 @@ TEMPLATE_CONTEXT_PROCESSORS = TCP+(
 DATABASES = {
         'default': {
             'ENGINE': 'django.contrib.gis.db.backends.postgis', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'recursosdb',                      # Or path to database file if using sqlite3.
+            'NAME': DBNAME,                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'recursosrh',
-            'PASSWORD': 'nmveaviieotf',
-            'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'USER': USNAME,
+            'PASSWORD': USCONTRA,
+            'HOST': IPDIR,                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                               # Set to empty string for default.
         }
     }
