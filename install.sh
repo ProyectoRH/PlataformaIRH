e13a5561fe9a6cc2fd1bf8ef3d1302e6d7d6975d
Mario Bonfante Aldana <maxboal@gmail.com>
2015-07-14 12:51:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -53,4 +53,22 @@ pip install six
 pip install wsgiref
 pip install gunicorn
 echo "*************************************************************************"
+echo "*************************************************************************"
+echo "* Cargando variables de configuración *"
+echo "*************************************************************************"
+read -p "El nombre de la base de datos: " dbname
+export dbname=$dbname
+sudo echo "export dbname=$dbname">> /etc/profile
+read -p "El nombre del usuario postgres: " usname
+export usname=$usname
+sudo echo "export usname=$usname">> /etc/profile
+read -p "La contraseña del usuario postgres: " uscontra
+export uscontra=$uscontra
+sudo echo "export uscontra=$uscontra">> /etc/profile
+read -p "La ip o nombre del servidor postgres: " ipdir
+export ipdir=$ipdir
+sudo echo "export ipdir=$ipdir">> /etc/profile
+echo "*******************************"
+echo "* Cargando modelos *"
+echo "*******************************"
 cd PlataformaIRH
